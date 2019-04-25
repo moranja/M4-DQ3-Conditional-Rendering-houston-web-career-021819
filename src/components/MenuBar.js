0f98f2cee054574799514d0392c7c8bfda39ca20
moranja <jeffadammoran@gmail.com>
2019-04-25 09:43:46 -0500

    done

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const MenuBar = (props) => {
+class MenuBar extends React.Component {
 
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -13,25 +13,33 @@ const MenuBar = (props) => {
 
   */
 
-  return (
-    <div className="ui four item menu">
-      <a className="item active" id="profile">
-        <i className="user large icon" id="profile"/>
-      </a>
+  handleClick = (e) => {
+    console.log(e)
+    e.persist()
+    this.props.changeActive(e.target.id)
+  }
 
-      <a className="item" id="photo">
-        <i className="photo large icon" id="photo"/>
-      </a>
+  render() {
+    return (
+      <div className="ui four item menu">
+        <a className={this.props.active === "profile" ? "item active" : "item"} id="profile" onClick={this.handleClick}>
+          <i className="user large icon" id="profile"/>
+        </a>
 
-      <a className="item" id="cocktail">
-        <i className="cocktail large icon" id="cocktail"/>
-      </a>
+        <a className={this.props.active === "photo" ? "item active" : "item"} id="photo">
+          <i className="photo large icon" id="photo" onClick={this.handleClick}/>
+        </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
-      </a>
-    </div>
-  )
+        <a className={this.props.active === "cocktail" ? "item active" : "item"} id="cocktail">
+          <i className="cocktail large icon" id="cocktail" onClick={this.handleClick}/>
+        </a>
+
+        <a className={this.props.active === "pokemon" ? "item active" : "item"} id="pokemon">
+          <i className=" themeisle large icon" id="pokemon" onClick={this.handleClick}/>
+        </a>
+      </div>
+    )
+  }
 
 }
 
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -4,21 +4,43 @@ import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
 
+  state = {
+    active: "profile"
+  }
+
+  changeActive = (value) => {
+    this.setState({
+      active: value
+    })
+  }
+
+  detailsToDisplay = () => {
+    if (this.state.active === "profile") {
+      return (<Profile />)
+    } else if (this.state.active === "photo") {
+      return (<Photos />)
+    } else if (this.state.active === "cocktail") {
+      return (<Cocktails />)
+    } else if (this.state.active === "pokemon") {
+      return (<Pokemon />)
+    }
+  }
 
   render() {
-
     /*
 
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+    Replace the code below! Depending on what menu item is selected in the menu,
+    I should render either a Profile, Photos, Cocktails, or Pokemon component.
+    Think of a way to track which menu item was selected. Which component should have state?
+    Which component should have methods to control state? Where should these methods be called?
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar changeActive={this.changeActive} active={this.state.active}/>
+        {this.detailsToDisplay()}
       </div>
     )
   }
